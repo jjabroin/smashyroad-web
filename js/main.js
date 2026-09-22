c6894ba2b365ff8064efb02257bc68f8754cb81c
leeseungyoon <leeseungyoon@users.noreply.github.com>
2026-09-22 15:47:47 +0900

    Version chain

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ import { createHUD, createInput, createBeeper, setSteerHint, fmtTime } from './h
 import { createGarage } from './garage.js?v=15';
 import { carSnapshot, blendSnapshot, extrapolateRemote, planOnlineGrid, STATE_HZ } from './net.js?v=8';
 import { createOnlinePanel } from './online.js?v=8';
-import { Board } from './board.js?v=1';
+import { Board } from './board.js?v=2';
 import { SkidTrails } from './skids.js?v=14';
 
 const canvas = document.getElementById('game');
